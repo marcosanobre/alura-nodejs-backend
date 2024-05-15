8ade30e0e12d6e1cf702c707faefe30a57525464
marcosanobre <marconobre@gmail.com>
2024-05-14 22:46:36 -0300

    Adição inicial de arquivos do Repo
    carga inicial , a partir ndo AcerNitro5 - casa

---
+++ erros.js
@@ -0,0 +1,22 @@
+// Erro de referencia
+// console.log( umaVariavel );
+
+// Erro de sintaxe
+// console.log( 'oi'
+
+let numero = 10;
+
+let nome = 'Alice';
+
+if ( numero === 10 ) {
+    console.log("O numero é 10.");
+} else {
+    console.log("O numero não é 10.");
+}
+
+
+if ( nome === "Alice" ) {
+    console.log("Boas vindas, Alice!");
+} else {
+    console.log("Você não é Alice.");
+}
---
+++ variaveis.js
@@ -0,0 +1,22 @@
+const estudante = 'Caroline';
+let professora;
+var umaVariavel = 'qualquer';
+professora  = 'AnaClaudia';
+
+// estudante = 'Marianna'; // Vai dar ERRO !!! 
+
+console.log( estudante, professora);
+
+/*
+ Esqueçe VAR
+ Sempre usar CONST quando não necessitar modificar o valor
+ CONST exige atribuição imediata, LET não.
+*/
+
+const minhaVar = 1;
+const MinhaVar = 'texto';
+const minhavar = '3';
+const MINHAVAR = 2;
+const minha_Var = 10;
+
+console.log( minhaVar, minhavar, MINHAVAR, MinhaVar, minha_Var);
