b09872c9c6b087a3f4551d5ab1cd4f3dfad7a7b0
Marcos Nobre <marconobre@gmail.com>
2024-05-21 12:19:37 -0300

    Atualização

--- modulo02-AULA06-listadeexercicios.js
+++ modulo02-AULA06-listadeexercicios.js
@@ -36,3 +36,56 @@ const frutasDeletadas = frutas.splice(2,2,'Kiwi','Pèssego');
 console.log( 'Frutas excluidas:', frutasDeletadas, '\n' );
 console.log( 'Array alterado: ', frutas, '\n\n' );
 
+
+// [4]
+/* 
+Crie dois arrays chamados menuPrincipal e menuDeSobremesas 
+contendo opções do cardápio de um restaurante. 
+Utilize o método concat para criar um novo array menuCompleto 
+contendo todos os elementos de menuPrincipal 
+seguidos pelos elementos de menuDeSobremesas.
+*/
+const menuPrincipal = ['Bife a cavalo', 'Estrogonofe de frango', 'Feijoada carioca'];
+const menuDeSobremesas = ['Pudin', 'Sorvete', 'Salada de frutas'];
+
+const menuCompleto = menuPrincipal.concat( menuDeSobremesas );
+console.log( menuCompleto, '\n\n' );
+
+
+// [5]
+/* 
+Crie uma lista bidimensional com 3 linhas e 3 colunas, 
+onde cada elemento seja uma matriz 3x3 com valores 
+iniciando em 1 e aumentando em 1 a cada elemento.
+*/
+var inc = 0;
+var matriz = [ [,,,], [,,,], [,,,] ];
+
+console.log( matriz, matriz.length , matriz[0].length);
+
+for (let c = 0; c < matriz.length; c++) {
+    for (let l = 0; l < matriz[c].length; l++) {
+        inc += 1;
+        matriz[c][l] = inc;
+    }
+}
+
+console.log( '\n\n', matriz );
+matriz.forEach( row => console.log( '\n', row ) );
+
+
+// [6]
+/* 
+Acesse e imprima o elemento na segunda linha e terceira coluna
+da lista bidimensional - matriz criada no exercicio anterior.
+*/
+
+console.log( '\n\nElemento da segunda linha e terceira coluna :', matriz[1][2] );
+
+
+// [7]
+/* Adicion 15 na 3ça linha e 2a coluna */
+matriz[2][1] = 15;
+
+console.log( '\n\nMatriz alterada :\n' );
+matriz.forEach( row => console.log( '\n', row ) );
