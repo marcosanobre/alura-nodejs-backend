b320951e38f4a360b1bd4ec38b7f809b78a131a8
Marcos Nobre <marconobre@gmail.com>
2024-05-27 14:35:29 -0300

    atualização

---
+++ modulo03-AULA02-1-objetosDeObjetos.js
@@ -0,0 +1,18 @@
+
+const estudante = {
+    nome: 'Jose Mane',
+    idade: 32,
+    cpf: '12345678901',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['+5561999999999','+5561988888888'],
+    media: 7.5
+};
+
+estudante.enderecos = [
+    {rua: 'Carmem Souza', numero: 33, cep: '71.540-133', complemento: 'Praça Teodoro, casa 18'},
+    {rua: 'Japao da Cruz', numero: null, cep: '70.054-321', complemento: 'Alameda dos Zumbis, casa 211'}
+];
+
+console.log( estudante );
+
---
+++ modulo03-AULA02-2-metodosDeObjetos.js
@@ -0,0 +1,20 @@
+
+const estudante = {
+    nome: 'Jose Mane',
+    idade: 32,
+    cpf: '12345678901',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['+5561999999999','+5561988888888'],
+    media: 7.5,
+    enderecos : [
+        {rua: 'Carmem Souza', numero: 33, cep: '71.540-133', complemento: 'Praça Teodoro, casa 18'},
+        {rua: 'Japao da Cruz', numero: null, cep: '70.054-321', complemento: 'Alameda dos Zumbis, casa 211'}
+    ],
+    estaAprovado: function( mediaBase) {
+        return this.media >= mediaBase ? true : false
+    }
+};
+
+console.log( estudante.estaAprovado(7), '\n\n' );
+
